f76fa1669835d835bc62deac34984e1d102b299e
zelr0x <zelr0xdev@gmail.com>
2021-08-12 02:43:19 +0400

    Added solution to 5.1

---
+++ ch5/e5_1.c
@@ -0,0 +1,55 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int getch(void) {
+    return getc(stdin);
+}
+void ungetch(int n) {
+    ungetc(n, stdin);
+}
+
+/* getint: get next integer from input into *pn */
+int getint(int *pn) {
+    int c, sign;
+    while (isspace(c = getch()))   /* skip white space */
+        ;
+    if (!isdigit(c) && c != EOF && c != '+' && c != '-') {
+        ungetch(c);   /* it is not a number */
+        return 0;
+    }
+    sign = (c == '-') ? -1 : 1;
+    if (c == '+' || c == '-') {
+        int prev = c;
+        c = getch();
+        if (!isdigit(c)) {
+            ungetch(c);
+            ungetch(prev);
+            return 0;
+        }
+    }
+    for (*pn = 0; isdigit(c); c = getch()) {
+        *pn = 10 * *pn + (c - '0');
+    }
+    *pn *= sign;
+    if (c != EOF) {
+        ungetch(c);
+    }
+    return c;
+}
+
+
+int main(void) {
+    int i = EOF;
+    getint(&i);
+    if (i == EOF) {
+        printf("[debug] i = EOF\n");
+    }
+    printf("[debug] i = %d\n", i);
+    const size_t sz = 128;
+    char buf[sz];
+    fgets(buf, sz, stdin);
+    printf("%s\n", buf);
+    return 0;
+}
+    
